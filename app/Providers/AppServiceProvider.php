99c4e19e7221daa4be343a93dcf2b3200e895906
ThanhThien07 <slen010207@gmail.com>
2026-07-05 19:29:14 +0700

    t

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Run migrations automatically on non-local environments (like Railway)
+        if (env('APP_ENV') !== 'local' && env('RUN_MIGRATIONS_ON_BOOT', true)) {
+            try {
+                $migrationFiles = glob(database_path('migrations/*.php'));
+                $hash = md5(implode('', $migrationFiles));
+
+                if (Cache::get('migrations_hash') !== $hash) {
+                    Artisan::call('migrate', ['--force' => true]);
+                    Cache::forever('migrations_hash', $hash);
+                }
+            } catch (\Throwable $e) {
+                // If cache fails (e.g. database not migrated yet), we run migrations anyway
+                try {
+                    Artisan::call('migrate', ['--force' => true]);
+                    try {
+                        Cache::forever('migrations_hash', md5(implode('', glob(database_path('migrations/*.php')))));
+                    } catch (\Throwable $ex) {}
+                } catch (\Throwable $ex) {
+                    // Ignore if database is not reachable/configured yet during build
+                }
+            }
+        }
     }
 }
